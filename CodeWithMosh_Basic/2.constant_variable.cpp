e752feed758ceae0e4321d8fb22bb44add3eadaf
AmirhosseinKhazaee <khazaeeaslamirhossein@gmail.com>
2026-04-25 22:46:43 +0330

    add constant_variable practice

--- CodeWithMosh_Basic/2.constant_variable.cpp
+++ CodeWithMosh_Basic/2.constant_variable.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 
-
+// muttable 
 int main(){
     double a = 11.1;
     std::cout << a;
+    std::cout << "\n";
     a = 12.1;
     std::cout << a;
 }
+
+
+// uncomment this to see the differece
+
+// immutable
+
+// int main(){
+//     const double a = 11.1;
+//     std::cout << a;
+//     std::cout << "\n";
+//     a = 12.1;
+//     std::cout << a;
+// }
